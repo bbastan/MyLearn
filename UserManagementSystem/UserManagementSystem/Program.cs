6f6278bebd1d37cf470f74ab60aa5bb087c8cb0f
bbastan <b.bastan12@outlook.com>
2025-05-26 00:58:25 +0330

    Version 1.0

---
+++ UserManagementSystem/UserManagementSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿using UserManagementSystem;
+
+User user = new("Ali",25,"ali123","a@123");
+
+try
+{
+    user.Login("ali123", "a@123");
+    user.DisplayInfo();
+    user.Logout();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ UserManagementSystem/UserManagementSystem/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagementSystem
+{
+    internal class User : Person, IUserOperations
+    {
+        public string Username { get; set; }
+        private string Password;
+
+        public User(string name, int age, string userName, string password)
+        {
+            Name = name;
+            Age = age;
+            Username = userName;
+            Password = password;
+        }
+        public void Login(string username, string password)
+        {
+            if (username == Username && password == Password)
+            {
+                Console.WriteLine("Login successful");
+            }
+            else
+            {
+                throw new Exception("Invalid usename or Pasword");
+            }
+        }
+        public void Logout() { Console.WriteLine($"{Username} :logged out"); }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+
+
+
+
+    }
+}
